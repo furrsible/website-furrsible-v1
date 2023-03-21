228f74cfc9c9faa14687067da21132b51fbd349e
elizabeth <leezeelola@gmail.com>
2023-03-21 17:57:24 +0100

    chore: add animate.css

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { GatsbyBrowser } from 'gatsby';
 import './src/common/styles/app.css';
+import 'animate.css';
 
 export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({
   element,
