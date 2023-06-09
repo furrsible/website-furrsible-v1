0353712b77f775478dcf423938f3c74282ec6d48
elizabeth <leezeelola@gmail.com>
2023-06-09 20:17:08 +0100

    update: add slider

--- src/common/components/Carousel/index.tsx
+++ src/common/components/Carousel/index.tsx
@@ -1,65 +1,93 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { A11y, Autoplay } from 'swiper';
+import Image from '../Image';
+
+import slide1 from '../../images/slide1.png';
+import slide2 from '../../images/slide2.png';
+import slide3 from '../../images/slide3.png';
+import slide4 from '../../images/slide4.png';
+import slide5 from '../../images/slide5.png';
 
 import 'swiper/css';
-
-function HideArrows() {
-  return <div className="hidden" />;
-}
+import 'swiper/css/a11y';
+import 'swiper/css/autoplay';
 
 const SimpleSlider = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 2,
-    autoplay: true,
-    speed: 1000,
-    autoplaySpeed: 3000,
-    cssEase: `linear`,
-    nextArrow: <HideArrows />,
-    prevArrow: <HideArrows />,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 5,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
   return (
-    <div className="text-white">
-      <Swiper spaceBetween={50} slidesPerView={5}>
-        <SwiperSlide>
-          <h3>1</h3>
+    <div className="text-white mt-16 lg:ml-14">
+      <Swiper
+        modules={[A11y, Autoplay]}
+        loop
+        rewind
+        breakpoints={{
+          320: {
+            slidesPerView: 2,
+
+            centeredSlides: true,
+          },
+          768: {
+            slidesPerView: 4,
+          },
+          1024: {
+            slidesPerView: 5,
+          },
+        }}
+        spaceBetween={10}
+        autoplay={{ delay: 2000 }}
+      >
+        <SwiperSlide className="relative">
+          <Image src={slide1} alt="a cat" />
+          <div
+            className="h-[54px] rounded-3xl mt-4 brightness-[.5]"
+            style={{
+              background: `linear-gradient(160.2deg, rgba(255, 255, 255, 0.1) 2.36%, rgba(255, 255, 255, 0) 100.6%)`,
+              transform: `matrix(1, -0.04, 0.01, 1, 0, 0) translate(0,10px) rotate(-2.22deg)`,
+            }}
+          />
         </SwiperSlide>
+
         <SwiperSlide>
-          <h3>2</h3>
+          <div
+            className="h-[54px] rounded-3xl mb-4 brightness-[.5]"
+            style={{
+              background: `linear-gradient(160.2deg, rgba(255, 255, 255, 0.1) 2.36%, rgba(255, 255, 255, 0) 100.6%)`,
+              transform: `matrix(1, -0.04, 0.01, 1, 0, 0) rotate(-2.22deg)`,
+            }}
+          />
+          <Image src={slide2} alt="a calico cat" />
         </SwiperSlide>
+
         <SwiperSlide>
-          <h3>3</h3>
+          <Image src={slide3} alt="woman holding a dog" />
+          <div
+            className="h-[54px] rounded-3xl mt-4 brightness-[.5]"
+            style={{
+              background: `linear-gradient(160.2deg, rgba(255, 255, 255, 0.1) 2.36%, rgba(255, 255, 255, 0) 100.6%)`,
+              transform: `matrix(1, -0.04, 0.01, 1, 0, 0) translate(0,10px) rotate(-2.22deg)`,
+            }}
+          />
         </SwiperSlide>
+
         <SwiperSlide>
-          <h3>4</h3>
+          <div
+            className="h-[54px] rounded-3xl mb-4 brightness-[.5]"
+            style={{
+              background: `linear-gradient(160.2deg, rgba(255, 255, 255, 0.1) 2.36%, rgba(255, 255, 255, 0) 100.6%)`,
+              transform: `matrix(1, -0.04, 0.01, 1, 0, 0) rotate(-2.22deg)`,
+            }}
+          />
+          <Image src={slide4} alt="a dog" />
         </SwiperSlide>
+
         <SwiperSlide>
-          <h3>5</h3>
-        </SwiperSlide>
-        <SwiperSlide>
-          <h3>6</h3>
+          <Image src={slide5} alt="a cat looking up" />
+          <div
+            className="h-[54px] rounded-3xl mt-4 brightness-[.5]"
+            style={{
+              background: `linear-gradient(160.2deg, rgba(255, 255, 255, 0.1) 2.36%, rgba(255, 255, 255, 0) 100.6%)`,
+              transform: `matrix(1, -0.04, 0.01, 1, 0, 0) translate(0,10px) rotate(-2.22deg)`,
+            }}
+          />
         </SwiperSlide>
       </Swiper>
     </div>
