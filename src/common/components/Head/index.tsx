92b26492ae3e56935beacde1cb00c07984ade138
elizabeth <leezeelola@gmail.com>
2023-06-13 13:21:45 +0100

    update: seo

--- src/common/components/Head/index.tsx
+++ src/common/components/Head/index.tsx
@@ -2,6 +2,10 @@ export default function Head() {
   return (
     <>
       <html lang="en" />
+      <meta
+        name="description"
+        content="Making pet parenting easy! Your one-stop hub for all pet services, right at your fingertips."
+      />
       <title>Furrsible</title>
     </>
   );
